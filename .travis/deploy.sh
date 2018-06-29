a433335a6cbde588b5e756e5cea99fa560747344
EliuX <eliecerhdz@gmail.com>
2018-06-28 23:51:04 -0500

    Exclude external remote Travis CI config

--- .travis/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import
-echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
-mvn deploy --settings .travis/maven-settings.xml -DskipTests=true -P release
