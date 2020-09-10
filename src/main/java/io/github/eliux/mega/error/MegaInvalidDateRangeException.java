7ba35408185d013a74ab2c1a96191c5001e391de
Cenyo <cenyo.medewou@gmail.com>
2020-09-10 14:09:38 +0200

    Delete useless classes

--- src/main/java/io/github/eliux/mega/error/MegaInvalidDateRangeException.java
+++
@@ -1,8 +0,0 @@
-package io.github.eliux.mega.error;
-
-public class MegaInvalidDateRangeException extends MegaException {
-
-    public MegaInvalidDateRangeException(String errorMessage) {
-        super(errorMessage);
-    }
-}
